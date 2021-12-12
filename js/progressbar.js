3efaadd423866e8eabd650c40f942a8dc2c714db
Stephanno <poporaduu@gmail.com>
2021-12-12 00:58:54 +0000

    fixed the skill bars

--- js/progressbar.js
+++ js/progressbar.js
@@ -1,30 +1,29 @@
 var fills = document.querySelectorAll(".progressbar_fill");
-console.log(fills)
-
-var progress = 80;
-var maxProgress = 100;
 
 function renderFills(){
-    
-    var percent = progress / maxProgress * 100;
+    var array = [90, 60, 40, 70, 30];
 
+    var count = 0;
     //UPDATE COLOR
-
-    //green
-    document.documentElement.style.setProperty('--bar-fill', '#819447');
-    document.documentElement.style.setProperty('--bar-top', '#617034');
-
-    if (percent <= 50){     //yellow
-        document.documentElement.style.setProperty('--bar-fill', '#de9f47');
-        document.documentElement.style.setProperty('--bar-top', '#bd853c');
-    }
-    if (percent <= 22){     //red
-        document.documentElement.style.setProperty('--bar-fill', '#bf3333');
-        document.documentElement.style.setProperty('--bar-top', '#a32c2c');
-    }
-
     fills.forEach(element => {
-        element.style.width = percent + "%";
+        element.style.width = array[Math.floor(count)] + "%";
+        count += 0.5;
+    });
+
+    array.forEach(function(value, i) {
+        console.log('--bar-fill' + i)
+        //green
+        document.documentElement.style.setProperty('--bar-fill' + i, '#819447');
+        document.documentElement.style.setProperty('--bar-top' + i, '#617034');
+
+        if (value <= 50){     //yellow
+            document.documentElement.style.setProperty('--bar-fill' + i, '#de9f47');
+            document.documentElement.style.setProperty('--bar-top' + i, '#bd853c');
+        }
+        if (value <= 22){     //red
+            document.documentElement.style.setProperty('--bar-fill' + i, '#bf3333');
+            document.documentElement.style.setProperty('--bar-top' + i, '#a32c2c');
+        }
     });
 }
 
